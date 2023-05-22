fdedfe3e311267f82ec5450609d40f866a61c444
eduard <eduard42rus@mail.ru>
2023-05-22 21:19:31 +0700

    Init commit

---
+++ src/main/java/com/example/mapemployeebook/MapemployeebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapemployeebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapemployeebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapemployeebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mapemployeebook/MapemployeebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapemployeebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapemployeebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
